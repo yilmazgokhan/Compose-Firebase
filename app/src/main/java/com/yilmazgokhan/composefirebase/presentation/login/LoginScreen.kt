d7be8621c99bd08a20a9b6ee4ccf0ad9f25bef23
yilmazgokhan <yilmaz_gokhan@yahoo.com>
2022-10-10 20:23:04 +0300

    Create login vm

--- app/src/main/java/com/yilmazgokhan/composefirebase/presentation/login/LoginScreen.kt
+++ app/src/main/java/com/yilmazgokhan/composefirebase/presentation/login/LoginScreen.kt
@@ -1,4 +1,17 @@
 package com.yilmazgokhan.composefirebase.presentation.login
 
-class LoginScreen {
+import android.annotation.SuppressLint
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Composable
+fun LoginScreen(
+    viewModel: LoginViewModel,
+    navigateToRegister: () -> Unit,
+    navigateToHome: () -> Unit,
+) {
+    val state by viewModel.uiState.collectAsState()
+
 }
--- app/src/main/java/com/yilmazgokhan/composefirebase/presentation/login/LoginViewModel.kt
+++ app/src/main/java/com/yilmazgokhan/composefirebase/presentation/login/LoginViewModel.kt
@@ -1,4 +1,31 @@
 package com.yilmazgokhan.composefirebase.presentation.login
 
-class LoginViewModel {
+import com.blankj.utilcode.util.LogUtils
+import com.yilmazgokhan.composefirebase.base.BaseViewModel
+import com.yilmazgokhan.composefirebase.base.IViewEvent
+import com.yilmazgokhan.composefirebase.base.IViewState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class LoginViewModel @Inject constructor(
+) : BaseViewModel<LoginViewState, LoginViewEvent>() {
+
+    init {
+        LogUtils.d("$this")
+    }
+
+    override fun createInitialState(): LoginViewState = LoginViewState()
+
+    override fun onTriggerEvent(event: LoginViewEvent) {
+        TODO("Not yet implemented")
+    }
 }
+
+sealed class LoginViewEvent : IViewEvent {
+    object LoginEvent : LoginViewEvent()
+}
+
+data class LoginViewState(
+    val isLoading: Boolean = false,
+) : IViewState
