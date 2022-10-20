5dd48934177af13c9f22356f5e19652a94db460a
yilmazgokhan <yilmaz_gokhan@yahoo.com>
2022-10-20 23:18:37 +0300

    Fix google login

--- app/src/main/java/com/yilmazgokhan/composefirebase/presentation/login/LoginScreen.kt
+++ app/src/main/java/com/yilmazgokhan/composefirebase/presentation/login/LoginScreen.kt
@@ -8,9 +8,11 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import com.blankj.utilcode.util.LogUtils
 import com.google.android.gms.auth.api.signin.GoogleSignIn
@@ -18,7 +20,9 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.GoogleAuthProvider
+import com.yilmazgokhan.composefirebase.AuthenticationState
 import com.yilmazgokhan.composefirebase.ui.component.ButtonDefault
+import com.yilmazgokhan.composefirebase.ui.component.DefaultScaffold
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
@@ -43,12 +47,18 @@ fun LoginScreen(
             }
         }
 
-    Column {
-        ButtonDefault(text = "Google Sign In",
-            click = {
-                googleLogin(context, launcher)
-            }
-        )
+    DefaultScaffold(loading = state.isLoading || state.loginState == AuthenticationState.IN_PROGRESS) {
+        Column(modifier = Modifier.padding(it)) {
+            ButtonDefault(text = "Google Sign In",
+                click = {
+                    googleLogin(context, launcher)
+                }
+            )
+        }
+    }
+
+    if (state.loginState == AuthenticationState.AUTHENTICATED) {
+        navigateToHome()
     }
 }
 
@@ -57,16 +67,12 @@ private fun googleLogin(
     launcher: ManagedActivityResultLauncher<Intent, ActivityResult>
 ) {
     lateinit var googleSignInClient: GoogleSignInClient
-
     val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-        .requestIdToken("854786605418-peltml5envocm8j3r0ca3inpm45ngd1p.apps.googleusercontent.com")
+        .requestIdToken("854786605418-68nkv5ep7n15euu0ngvumbm4map7l8pp.apps.googleusercontent.com")
         .requestEmail()
         .build()
 
     googleSignInClient = GoogleSignIn.getClient(context, gso)
-
     val signInIntent = googleSignInClient.signInIntent
-
     launcher.launch(signInIntent)
-
 }
