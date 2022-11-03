fac9d4a516805cdd6ff074b0c4ff02ec88c8bd94
yilmazgokhan <yilmaz_gokhan@yahoo.com>
2022-11-03 23:53:48 +0300

    Change google button ui

--- app/src/main/java/com/yilmazgokhan/composefirebase/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/yilmazgokhan/composefirebase/presentation/home/HomeScreen.kt
@@ -4,6 +4,8 @@ import androidx.activity.compose.BackHandler
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import com.yilmazgokhan.composefirebase.ui.component.DefaultScaffold
+import com.yilmazgokhan.composefirebase.ui.component.DefaultToolbar
 import com.yilmazgokhan.composefirebase.ui.component.TextSecondary
 
 @Composable
@@ -16,5 +18,15 @@ fun HomeScreen(
         // TODO:
     })
 
-    TextSecondary(text = "Temp")
+    DefaultScaffold(topBar = { HomeTopBar() }) {
+
+    }
+}
+
+@Composable
+fun HomeTopBar() {
+    DefaultToolbar(
+        title = "Home",
+
+    )
 }
--- app/src/main/java/com/yilmazgokhan/composefirebase/presentation/login/LoginScreen.kt
+++ app/src/main/java/com/yilmazgokhan/composefirebase/presentation/login/LoginScreen.kt
@@ -7,20 +7,27 @@ import androidx.activity.compose.ManagedActivityResultLauncher
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.blankj.utilcode.util.LogUtils
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.GoogleAuthProvider
-import com.yilmazgokhan.composefirebase.ui.component.ButtonDefault
+import com.yilmazgokhan.composefirebase.R
 import com.yilmazgokhan.composefirebase.ui.component.DefaultScaffold
 import com.yilmazgokhan.composefirebase.util.Constants.Firebase.CLIENT_ID
 import com.yilmazgokhan.composefirebase.util.NavigateTo
@@ -50,12 +57,30 @@ fun LoginScreen(
         }
 
     DefaultScaffold(loading = state.isLoading) {
-        Column(modifier = Modifier.padding(it)) {
-            ButtonDefault(text = "Google Sign In",
-                click = {
+        Column(modifier = Modifier
+            .padding(it)
+            .fillMaxSize(),
+            verticalArrangement = Arrangement.Center) {
+            Button(
+                onClick = {
                     googleLogin(context, launcher)
-                }
-            )
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 16.dp, end = 16.dp),
+                shape = RoundedCornerShape(6.dp),
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = Color.Black,
+                    contentColor = Color.White
+                )
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_google_logo),
+                    contentDescription = "",
+                    tint = Color.Unspecified
+                )
+                Text(text = "Sign in with Google", modifier = Modifier.padding(6.dp))
+            }
         }
     }
 
