65d0ff0862bfb5d226449c54816b30fd99037a86
yilmazgokhan <yilmaz_gokhan@yahoo.com>
2023-01-06 00:16:37 +0300

    Call GetUserUseCase

--- app/src/main/java/com/yilmazgokhan/composefirebase/presentation/profile/ProfileViewModel.kt
+++ app/src/main/java/com/yilmazgokhan/composefirebase/presentation/profile/ProfileViewModel.kt
@@ -1,10 +1,12 @@
 package com.yilmazgokhan.composefirebase.presentation.profile
 
 import androidx.lifecycle.viewModelScope
+import com.blankj.utilcode.util.LogUtils
 import com.yilmazgokhan.composefirebase.base.BaseViewModel
 import com.yilmazgokhan.composefirebase.base.IViewEvent
 import com.yilmazgokhan.composefirebase.base.IViewState
 import com.yilmazgokhan.composefirebase.domain.usecase.GetUserUseCase
+import com.yilmazgokhan.composefirebase.util.State
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -16,11 +18,30 @@ class ProfileViewModel @Inject constructor(
 
     override fun createInitialState(): ProfileViewState = ProfileViewState()
 
+    init {
+        getUser()
+    }
     override fun onTriggerEvent(event: ProfileViewEvent) {
         viewModelScope.launch {
             when (event) {
                 ProfileViewEvent.ProfileEvent -> {
+                    getUser()
 
+                }
+            }
+        }
+    }
+
+    private fun getUser() {
+        viewModelScope.launch {
+            when(val result = getUserUseCase.execute(input = null)) {
+                is State.Success -> {
+                    result.data
+                    LogUtils.d("${result.data}")
+                }
+                is State.Error -> {
+                    LogUtils.d("${result.exception}")
+                    result.exception
                 }
             }
         }
