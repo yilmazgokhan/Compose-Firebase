6a4cb76ac743e1c0ad1d1405058f18ea24c749cb
yilmazgokhan <yilmaz_gokhan@yahoo.com>
2023-02-13 22:25:20 +0300

    Create lazy column

--- app/src/main/java/com/yilmazgokhan/composefirebase/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/yilmazgokhan/composefirebase/presentation/home/HomeScreen.kt
@@ -2,11 +2,18 @@ package com.yilmazgokhan.composefirebase.presentation.home
 
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.yilmazgokhan.composefirebase.R
+import com.yilmazgokhan.composefirebase.data.repository.model.Chat
 import com.yilmazgokhan.composefirebase.ui.component.ButtonWithBorder
 import com.yilmazgokhan.composefirebase.ui.component.DefaultScaffold
 import com.yilmazgokhan.composefirebase.ui.component.ToolbarWithEndIcon
@@ -25,8 +32,7 @@ fun HomeScreen(
     })
 
     DefaultScaffold(topBar = { HomeTopBar(navigateToProfile) },
-        loading = state.isLoading) {
-        Column() {
+        bottomBar = {
             ButtonWithBorder(
                 text = "Create New Chat",
                 textColor = Color.White,
@@ -34,15 +40,29 @@ fun HomeScreen(
                 backgroundColor = Purple200,
                 click = { navigateToCreateChat() }
             )
+        },
+        loading = state.isLoading) {
+        Column() {
+            LazyColumn(modifier = Modifier.padding(horizontal = 4.dp)) {
+                items(items = state.chats) {
+                    RenderItem(it)
+                }
+            }
         }
     }
 }
 
 @Composable
-fun HomeTopBar(endIconClick: () -> Unit) {
+private fun HomeTopBar(endIconClick: () -> Unit) {
     ToolbarWithEndIcon(
         title = "Home",
         endIconRes = R.drawable.ic_baseline_person_24,
         endIconClick = endIconClick
     )
+}
+
+@Composable
+private fun RenderItem(chat: Chat) {
+    Text(text = chat.title ?: "")
+
 }
